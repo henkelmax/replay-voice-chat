5345206b2319d60bc45cd7cad7564a70de4fe8fc
mjwells <36360408+mjwells2002@users.noreply.github.com>
2022-03-11 15:05:55 +0000

    Fix fix replay time skipping

--- src/main/java/xyz/breadloaf/replaymodinterface/ReplayInterface.java
+++ src/main/java/xyz/breadloaf/replaymodinterface/ReplayInterface.java
@@ -19,7 +19,6 @@ public class ReplayInterface implements ClientModInitializer {
     public boolean isInReplayEditor;
     @Nullable
     public ReplayHandler replayHandler;
-    public boolean skipping;
 
     public ReplayInterface() {
         if (INSTANCE == null) {
